947cd085659cdb9f5d7cffbecb729e52920e5c12
varshredi <80800383+varshredi@users.noreply.github.com>
2022-03-07 22:35:43 +0530

    Lessonn

---
+++ Phase1 Lesson5/Phase1 Lesson5/InsertionSort/src/com/varshitha/assignment/InsertionSort.java
@@ -0,0 +1,25 @@
+package com.varshitha.assignment;
+
+public class InsertionSort {
+	public static void main(String[] args){
+		 int[] arr = {9,12,3,21,44};
+		 InsertionSort(arr);
+		 for(int i=0;i<arr.length;i++){
+		 System.out.println(arr[i]);
+		 }
+		 }
+		 public static void InsertionSort(int[] arr){
+		 int len = arr.length;
+		 for(int j=1;j<len;j++){
+		 int key = arr[j];
+		 int i=j-1;
+		 while ((i>-1) && (arr[i]>key)){
+		 arr[i+1]=arr[i];
+		 i--;
+		 }
+		 arr[i+1]=key;
+		 }
+		 }
+
+
+}
---
+++ Phase1 Lesson5/Phase1 Lesson5/InsertionSort/src/module-info.java
@@ -0,0 +1,2 @@
+module InsertionSort {
+}
