a006d5d39b6186f46ad3343529c48c3b092f4c16
varshredi <80800383+varshredi@users.noreply.github.com>
2022-03-07 22:41:59 +0530

    Lessonn

---
+++ ExponentialSearch/src/com/varshitha/assignment/ExponentialSearch.java
@@ -0,0 +1,30 @@
+package com.varshitha.assignment;
+import java.util.Arrays;
+public class ExponentialSearch {
+	
+	
+	public static void main(String[] args){
+	int[] arr = {6,12,18,24,32};
+	int length= arr.length;
+	int value = 18;
+	int outcome = ExponentialSearch(arr,length,value);
+	if(outcome<0){
+	System.out.println( "Element is not present in the array");
+	}
+	else
+	{
+	System.out.println( "Element is present in the array at index:"+outcome);
+	}
+	}
+	public static int ExponentialSearch(int[] arr ,int length, int value ){
+	if(arr[0]==value){
+	return 0;
+	}
+	int i=1;
+	while(i<length && arr[i]<=value){
+	i=i*2;
+	}
+	return Arrays.binarySearch(arr,i/2,Math.min(i,length),value);
+	}
+	
+}
